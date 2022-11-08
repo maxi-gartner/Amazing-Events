9ff5a422d6c1c7511ea98cdc87704f17679d1be7
Maximiliano Gartner <maxi-gartner@hotmail.com>
2022-11-08 14:01:57 -0300

    :ok_hand: change order footer

--- data/home_data.js
+++ data/home_data.js
@@ -186,7 +186,7 @@ const data={
                 <p class="card-text">${all_data[i].description}</p>
                 <div class="price_and_buttom">
                 <p>Price: ${all_data[i].price}</p>
-                <a href="./details.html" class="btn btn-primary">See more</a>
+                <a href="./pages/details.html" class="btn btn-primary">See more</a>
                 </div>
                 </div>
             </div>
